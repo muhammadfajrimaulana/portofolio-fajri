f2355695e0b4eb4f13e409696ea820901e60e7c1
muhammadfajrimaulana <fajrimaulanamaulana008@gmail.com>
2024-07-18 16:48:37 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->set404Override(function() {
+    echo view('errors/not_found');
+    });
